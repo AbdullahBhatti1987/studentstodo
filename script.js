756481de4341c1acad69c4a4fbf0227ad47390f0
AbdullahBhatti1987 <abdullah.bhatti345@gmail.com>
2024-06-15 01:22:23 +0500

    students todo final
    Students todo complete Al Hamdulillah.
    assignment by sir Bilal SMIT dated 13-06-2024.

--- script.js
+++ script.js
@@ -1,37 +1,85 @@
 var taskData = [
-    {
-    task: '1',
-    date: '2024-06-15',
-    category: 'Homework',
-},{
-    task: '2',
-    date: '2024-06-17',
-    category: 'Exams',
-},{
-    task: '3',
-    date: '2024-06-19',
-    category: 'Projects',
-},{
-    task: '4',
-    date: '2024-06-21',
-    category: 'Extracurricular',
-},{
-    task: '5',
-    date: '2024-06-23',
-    category: 'Personal',
-},{
-  task: '6',
-  date: '2024-06-22',
-  category: 'Personal',
-},{
-  task: '7',
-  date: '2024-06-21',
-  category: 'Personal',
-},{
-  task: '8',
-  date: '2024-06-20',
-  category: 'Personal',
-}
+  {
+      task: 'Attend Math Lecture',
+      date: '2024-06-15',
+      category: 'Homework',
+  },{
+      task: 'Study for History Exam',
+      date: '2024-06-17',
+      category: 'Exams',
+  },{
+      task: 'Work on Science Project',
+      date: '2024-06-19',
+      category: 'Projects',
+  },{
+      task: 'Practice Football',
+      date: '2024-06-21',
+      category: 'Extracurricular',
+  },{
+      task: 'Read a Book',
+      date: '2024-06-23',
+      category: 'Personal',
+  },{
+      task: 'Go for a Run',
+      date: '2024-06-22',
+      category: 'Personal',
+  },{
+      task: 'Attend Music Class',
+      date: '2024-06-21',
+      category: 'Personal',
+  },{
+      task: 'Write in Journal',
+      date: '2024-06-20',
+      category: 'Personal',
+  },{
+      task: 'Complete Math Assignment',
+      date: '2024-06-16',
+      category: 'Personal',
+  },{
+      task: 'Prepare for Science Presentation',
+      date: '2024-06-18',
+      category: 'Projects',
+  },{
+      task: 'Research for History Paper',
+      date: '2024-06-20',
+      category: 'Homework',
+  },{
+      task: 'Attend Physics Lab',
+      date: '2024-06-15',
+      category: 'Projects',
+  },{
+      task: 'Read for English Literature',
+      date: '2024-06-17',
+      category: 'Homework',
+  },{
+      task: 'Practice for Soccer Match',
+      date: '2024-06-19',
+      category: 'Extracurricular',
+  },{
+      task: 'Study for Biology Test',
+      date: '2024-06-21',
+      category: 'Exams',
+  },{
+      task: 'Attend Photography Workshop',
+      date: '2024-06-22',
+      category: 'Personal',
+  },{
+      task: 'Write Poetry',
+      date: '2024-06-21',
+      category: 'Personal',
+  },{
+      task: 'Create Art Project',
+      date: '2024-06-20',
+      category: 'Projects',
+  },{
+      task: 'Plan for Vacation',
+      date: '2024-06-16',
+      category: 'Personal',
+  },{
+      task: 'Practice Guitar',
+      date: '2024-06-18',
+      category: 'Personal',
+  }
 ];
 
 const addTaskBtn = () => {
@@ -83,45 +131,70 @@ const render = (filterData) => {
     taskList.innerHTML = "";
     filterData.forEach(function (data, ind) {
     const task = `<li>
-        <span class="id">${data.task}</span>
+        <span class="id${ind + 1}">${ind + 1}</span>
+        <span class="task">${data.task}</span>
         <span class="dueDate">${data.date}</span>
         <span class="category">${data.category}</span>
-        <div class="buttonDiv">
-        <button onclick="editTask(this)">Edit</button>
-        <button onclick="removeTask(this)">Remove</button>
-        </div>
+          <div class="buttonDiv">
+            <button onclick="editTask(this)">Edit</button>
+            <button onclick="removeTask(this)">Remove</button>
+          </div>
         </li>`;
         taskList.innerHTML += task;
         });
-        }
+}
 
-// const removeTask = (element) => {
-//     const address = element.parentElement.parentElement;
-// taskData.some(function(data,ind){
-//     if(data.task === address.children[0].innerText){
-//         console.log('value matched')
-//         taskData.splice(ind, 1)
-//     }   
+const removeTask = (element) => {
+    const address = element.parentElement.parentElement;
+  taskData.some(function(data,ind){
+    if(data.task === address.children[1].innerText && data.category === address.children[3].innerText){
+        taskData.splice(ind, 1)
+    }   
+});
+render(taskData)
+}
 
-// })
-    
-//         const filterItems = taskData.filter(data => 
-//             data.category === element.value || element.value === "All");
-//         render(filterItems);
-    
-//     // render(taskData)
-//     // filterCategory
-// }
+const editTask = (element) => {
+    const address = element.parentElement.parentElement.children;
+    document.getElementById("taskName").value = address[1].innerText;
+    document.getElementById("dueDate").value = address[2].innerText;
+    document.getElementById("category").value = address[3].innerText;
+    taskData.some(function(data,ind){
+      if(data.task === address[1].innerText && data.category === address[3].innerText){
+        taskData.splice(ind, 1);
+      }
+  })
+}
 
-// const editTask = (element) => {
-//     const address = element.parentElement.parentElement;
-//     const updatedValue = prompt('Enter Updated Value', 'Enter Updated Value')
-// taskData.some(function(data,ind){
-//     taskData.task = updatedValue;
-//     })
-//     render(taskData)
-// }
 
+document.getElementById('nameUp').addEventListener('click', ()  => {
+  taskData.sort((a, b) => (a.task > b.task ? 1 : -1));
+  render(taskData)
+});
+
+document.getElementById('nameDown').addEventListener('click', () => {
+  taskData.sort((a, b) => (a.task > b.task ? -1 : 1));
+  render(taskData)
+});
+
+document.getElementById('dateDown').addEventListener('click', ()  => {
+  taskData.sort((a, b) => (a.date > b.date ? 1 : -1));
+  render(taskData)
+});
+
+document.getElementById('dateUp').addEventListener('click', () => {
+  taskData.sort((a, b) => (a.date > b.date ? -1 : 1));
+  render(taskData)
+});
+
+document.getElementById('categoryDown').addEventListener('click', ()  => {
+  taskData.sort((a, b) => (a.category > b.category ? 1 : -1));
+  render(taskData)
+});
+
+document.getElementById('categoryUp').addEventListener('click', () => {
+  taskData.sort((a, b) => (a.category > b.category ? -1 : 1));
+  render(taskData)
+});
 
 render(taskData);
-
